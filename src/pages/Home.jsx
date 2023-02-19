012c87bbd30e74712bc5430bf35562ace32aeacc
MehakK1311 <mehakhanna1311@gmail.com>
2023-02-19 12:00:55 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import Searchbar from "../components/Searchbar";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
+import Property from "../components/Property";
 import { Flex } from "@chakra-ui/react";
+import SMALL_DATA from "../utils/SMALL_DATA.json";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Searchbar />
       <Banner
         purpose="BUY A HOME"
         title1=" Find, Buy & Own Your"
@@ -19,9 +19,19 @@ const Home = () => {
         linkName="/search"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <Flex flexWrap="wrap">
-        {/* {propertiesForSale.map((property) => (
-          <Property property={property} key={property.id} /> */}
+
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+        {SMALL_DATA.map((data) => (
+          <Property
+            coverPhoto={"../assets/images/house.jpg"}
+            price={data.price}
+            rooms={data.beds}
+            title={data.name}
+            baths={data.bathrooms}
+            contact={data.contact}
+            key={data.id}
+          />
+        ))}
       </Flex>
       <Footer />
     </div>
